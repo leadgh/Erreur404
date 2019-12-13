1a995c608457e4d9a856d55cfdf06628e0badb96
leadgh <leadgh@users.noreply.github.com>
2019-12-13 14:21:37 +0100

    1 er commit

--- js/index.js
+++ js/index.js
@@ -1,13 +1,32 @@
-//Ouvrir la sidenav
-function openNav() {
-  document.getElementById("mySidenav").style.width = "25%";
-  document.getElementById("main").style.marginLeft = "25%";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-}
 
+//test par alerte taille de la fenêtre
+var message = 'La taille de votre fenêtre est de ' + window.innerWidth + 'x' + window.innerHeight + '.';
+if (window.innerWidth >= 940){
+    message += "Il est au format desktop";
+} else {
+    message += "Il est au format mobile";
+}
+alert(message);
+
+if(window.innerWidth > 940){
+  //Ouvrir la sidenav dans le contenu
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "25%";
+    document.getElementById("main").style.marginLeft = "25%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+} else if(window.innerWidth < 940){ 
+  //Ouvrir la sidenav sur le contenu
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "50%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+}
 //Fermer la sidenav
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
   document.body.style.backgroundColor = "white";
 }
+
+//Accordéon
